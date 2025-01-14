d9c85365f8deb197b5a0f6a76daa422af40dd7ca
Eklerka25 <134164161+Eklerka25@users.noreply.github.com>
2025-01-14 21:06:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,3 +10,20 @@ TOKEN_TYPE_SEMICOLON = "SCOL"
 TOKEN_TYPE_INT = "INT"
 
 data = "int x = 10;"
+
+t = list(data)
+
+lexed_data = {}
+string_mode = False
+token_int = 1
+
+for x in range(len(t)):
+    if t[x] == " ":
+        token_int += 1
+    elif t[x] == ";":
+        token_int = 0;
+    else:
+        lexed_data.setdefault(token_int, "")
+        lexed_data[token_int] += t[x]
+
+print(lexed_data)
